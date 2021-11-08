f6d46275f1a148cd29d67a37052123f14fcb5123
ChebotarRoman <93655111+ChebotarRoman@users.noreply.github.com>
2021-11-08 21:22:11 +0300

    reload

--- Task2.java
+++ Task2.java
@@ -9,51 +9,40 @@ class Task2 {
 	sumWholeNumbers(1, 3);
 	sumWholeNumbers(10, 5);
 	sumWholeNumbers(21, 43);
-	transferredNumPosOrNeg (-7);
-	transferredNumPosOrNeg (56);
-	transferredWholeNum (101);
-	transferredWholeNum (0);
-	transferredWholeNum (-212);
-	passString ();
-	leapYear (88);
-	leapYear (100);
-	leapYear (400);
-	leapYear (800);
-	leapYear (99);
-	leapYear (1000);
-	leapYear (717);
+	transferredNumPosOrNeg(-7);
+	transferredNumPosOrNeg(56);
+	transferredWholeNum(101);
+	transferredWholeNum(0);
+	transferredWholeNum(-212);
+	passString("lesson2. Task4", 5);
+	System.out.println(leapYear(88));
+	System.out.println(leapYear (100));
+	System.out.println(leapYear (400));
+	System.out.println(leapYear (800));
+	System.out.println(leapYear (99));
+	System.out.println(leapYear (1000));
+	System.out.println(leapYear (717));
     }
     
-	static void sumWholeNumbers (int a, int b) {
-	System.out.println(10 >= a + b && a + b <= 20? "true" : "false");
+	static boolean sumWholeNumbers (int a, int b) {
+		return 10 >= a + b && a + b <= 20;
 	}
 	
 	static void transferredNumPosOrNeg (int a) {
-	/*if (a >= 0) {
-	/	System.out.println("positive");
-	} else {
-		System.out.println("negative");
-	}*/
-	System.out.println("Number is " + (a >= 0? "positive" : "negative")); 
+		System.out.println("Number is " + (a >= 0? "positive" : "negative")); 
 	}
 	
-	static void transferredWholeNum (int a) {
-		System.out.println(a < 0? "true" : "false");
+	static boolean transferredWholeNum (int a) {
+		return a < 0;
 	}
 	
-    static void passString () {
-		for (int x = 0; x < 5; x++) {
-		System.out.println("Java1.Lesson2.Task4");
+    static void passString(String x, int count) {
+		for (int i = 0; i < count; i++) {
+		System.out.println(x);
 		}
 	}
 	
-	static void leapYear (int a) {
-		if (a % 4 == 0) {
-		if ((a % 100 != 0) || (a % 400 == 0)) {
-			System.out.println("true");
-			} else {
-			System.out.println("false");
-			}
-		}
+	static boolean leapYear(int year) {
+		return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 	}
 }
